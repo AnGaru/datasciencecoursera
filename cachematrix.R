4ac9595f63afb4ddf4fa03ff0c87a3af4d51ee26
AnGaru <garucellbop@gmail.com>
2020-11-21 14:11:55 +0100

    Corrected exercise

--- cachematrix.R
+++ cachematrix.R
@@ -14,7 +14,7 @@ makeCacheMatrix <- function(x = matrix()) {
     inv <<- NULL
   }
   get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
+  setinverse <- function(inverse) inv <<- solve
   getinverse <- function() inv
   list(set = set, get = get,
        setinverse = setinverse,
